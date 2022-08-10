641a061f78b608dd7784b3667e45ce23fbe6afc8
lisarah <crab.apples00@gmail.com>
2022-08-10 09:33:43 -0700

    Added visualization code for the tolled MDP queue game

--- V3/solve_tolled_queued_game.py
+++ V3/solve_tolled_queued_game.py
@@ -16,17 +16,18 @@ import util.utilities as ut
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import models.test_model as test
-
+import pickle
 
 epsilon = 100 # error in approximate solution of drivers learning
 # epsilon_list = [5e3]  # [1e5, 2e4, 2e3, 1e3]
 borough = 'Manhattan' # borough of interest
 mass = 10000 # game population size
 constrained_value = 350 # 350  for flat # maximum driver density per state
-# max_error = 5000
-max_errors = [10000, 5000, 1000, 500, 100]
-max_iterations = 1000 # number of iterations of dual ascent
+max_errors = [5000]#[10000, 5000, 1000, 500, 100]
+max_iterations = 10 # number of iterations of dual ascent
 toll_queues = False
+save_last_toll_results = True
+save_plots = False
 # game definition with initial distribution
 # print(f'cur seed {np.random.get_state[1][0]}')
 np.random.seed(3239535799)
@@ -35,25 +36,24 @@ manhattan_game = queued_game.queue_game(mass, 0.01, uniform_density=True,
 
 initial_density = manhattan_game.get_density()
 y_res, obj_hist = fw.FW_dict(manhattan_game, max_errors[-1], max_iterations)
-# determine the step size, which depends on strong convexity 
-# and the constraint norm
-alpha = manhattan_game.get_strong_convexity()
-print(f'convexity factor is {alpha}')
 
-# set constraints
-T = len(manhattan_game.forward_P)
+# set constraints 
 constrained_zones = [161, 162, 236, 237] #[161,43,68,79,231,236,237,114]# manhattan_game.z_list
-Z = len(constrained_zones)
-
-# constrained_states = [(c, 0) for c in constrained_zones]
 manhattan_game.set_constraints(constrained_zones, constrained_value)
 grad, violation_norm = manhattan_game.get_constrained_gradient(
     y_res[-1], return_violation = True)
 print(f'before tolling: violation_density = {violation_norm}')
 
+
+# step size calculations. Step size =  alpha / 2 / |A|^2_2
+# alpha is alpha strong convexity, |A|^2_2 is the two norm of constraint matrix
+# and the constraint norm
+alpha = manhattan_game.get_strong_convexity()
+print(f'convexity factor is {alpha}')
+T = len(manhattan_game.forward_P)
+Z = len(constrained_zones)
 ZA = sum([len(manhattan_game.action_dict[(z, 0)]) for z in constrained_zones])
 A_arr = np.zeros((T*Z, ZA*T))  
-# sa_ind = 0
 za_ind = 0
 actions = manhattan_game.action_dict.values()
 for t in range(T):
@@ -67,7 +67,9 @@ two_norm_A = np.linalg.norm(A_arr,2)
 step_size = alpha/2/(two_norm_A**2)
 print(f'norm of A is {two_norm_A}, step size is {step_size}')
 # step_size = 0.12 #  0.0001
-# set initial toll value      
+
+
+# Initialize toll value at tolled states
 tau = {}
 for s in constrained_zones:
     tau.update({((s, 0), t): 0 for t in range(T)})
@@ -76,47 +78,37 @@ for s in constrained_zones:
 avg_tolls = {err: None for err in max_errors}
 avg_violations = {err: None for err in max_errors}
 
+# loop for running inexact pga at different error accuracies
 for err in max_errors:
     print('')
     print(f'------ running error {err} ---------')
 
     # bookkeeping stats
-    tau_hist = []
-    violation_hist = []
-    average_tau = {z: 0 for z in tau.keys()}
     avg_distribution = [{sa: 0 for sa in y_res[-1][t].keys()} 
                         for t in range(T)]
     avg_violation = []
     social_cost = []
     # define dual ascent approximate gradient update.
-    def approx_gradient(game, cur_tau, approx_err, k): # this eps comes from inexact_pga
+    # input approx_err comes from inexact_pga
+    def approx_gradient(game, cur_tau, approx_err, k): 
         game.update_tolls(cur_tau)
         # solve first game
-        # print(f'{approx_err}')
         approx_y, obj_hist = fw.FW_dict(game, approx_err, max_iterations, 
                                         initial_density, verbose=False)  
-        # average tau
-        for z in cur_tau.keys():
-            average_tau[z] = (average_tau[z]*k+cur_tau[z])/(k+1) 
-        # average distribution
-        # distribution_diff = 0
         for t in range(T):
             for sa in approx_y[-1][t].keys():
                 avg_distribution[t][sa] = (
                     avg_distribution[t][sa]*k+approx_y[-1][t][sa])/(k+1)
-                # distribution_diff += abs(avg_distribution[t][sa] - 
-                #                          approx_y[-1][t][sa])
-        # print(f' {k}: distribution difference {round(distribution_diff,2)}')
+                
         gradient, violation = manhattan_game.get_constrained_gradient(
             approx_y[-1], return_violation=True)
         _, avg_violation_k = manhattan_game.get_constrained_gradient(
             avg_distribution, return_violation=True)
         avg_violation.append(avg_violation_k)
         social_cost.append(game.get_social_cost(avg_distribution))
-        tau_arr = np.array(list(cur_tau.values()))
-        tau_norm = np.linalg.norm(tau_arr, 2)
-        print(f'\r {k}: average violation {round(avg_violation[-1],2)}, tau norm {tau_norm}   ', end ='')  
-        
+        tau_norm = np.linalg.norm(np.array(list(cur_tau.values())), 2)
+        print(f'\r {k}: average violation {round(avg_violation[-1],2)}, '
+              f'tau norm {tau_norm}   ', end ='')  
         return gradient
     
     tau_hist, gradient_hist = pga.inexact_pga(manhattan_game, tau,
@@ -148,17 +140,20 @@ for err in max_errors:
     plt.yscale('log')
     plt.grid()
     plt.tight_layout()
-    plt.savefig(f'toll_norm_convergence_{err}.png')
+    if save_plots:
+        plt.savefig(f'toll_norm_convergence_{err}.png')
     
     
     # social cost plot
     pt.objective(social_cost, social_cost[0], 'Social Cost for driver')  
     plt.tight_layout()
-    plt.savefig(f'grad_res/social_driver_profit_{err}.png')
+    if save_plots:
+        plt.savefig(f'grad_res/social_driver_profit_{err}.png')
     
     avg_tolls[err] = tau_values[-1]
     avg_violations[err] = avg_violation[-1]
-    
+
+# plot errors vs last average toll value and last violation violation
 x_axis_labels = [e /200839.1820886145 for e in max_errors]
 if len(max_errors) > 1:
     fig_width = 5.3 * 2
@@ -185,3 +180,24 @@ if len(max_errors) > 1:
     plt.grid()
     plt.subplots_adjust(hspace=.05)    
     plt.show()
+
+if save_last_toll_results:
+    open_file = open('grad_res/err_5000_toll_results.pickle', 'wb')
+    err_results = {'max_error': 5000,
+                   'tau_hist': tau_hist,
+                   'gradient_hist':gradient_hist,
+                   'avg_violation': avg_violation,
+                   'step_size': step_size,
+                   'social_cost': social_cost,
+                   'last_average_density': avg_distribution}
+    pickle.dump(err_results, open_file)
+    open_file.close()
+    
+# plot the summary plot of the tolled average results
+z_density = manhattan_game.get_zone_densities(avg_distribution, False)
+constraint_violation, violation_density = manhattan_game.get_violations(
+    z_density, constrained_value)
+avg_density = manhattan_game.get_average_density(z_density)
+
+visual.summary_plot(z_density, constraint_violation, violation_density, 
+                    avg_density, constrained_value, T)
